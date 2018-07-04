d23a4e293309f81eec48d4eff9a67b8c3fb58f15
asm32net <379754432@qq.com>
2018-07-04 13:21:38 +0800

    PaSwitchGitHubAccount2018JSC.js ... add

---
+++ PaSwitchGitHubAccount2018JSC.js/PaSwitchGitHubAccount2018JSC.js
@@ -0,0 +1,9 @@
+// PaSwitchGitHubAccount2018JSC.js
+
+import System;
+import System.Windows.Forms;
+import Accessibility;
+
+(function(){
+	MessageBox.Show("PaSwitchGitHubAccount2018JSC.js");
+})();
