343fc4bc1dc553cde5b89dd6b5fab7d2f1b0529f
asm32net <379754432@qq.com>
2018-07-04 13:21:21 +0800

    PaSwitchGitHubAccount2018JSC--linker.bat ... add

---
+++ PaSwitchGitHubAccount2018JSC.js/PaSwitchGitHubAccount2018JSC--linker.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set strLinker=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\jsc.exe
+set strAppName=PaSwitchGitHubAccount2018JSC
+
+echo %strLinker%
+echo.
+
+set strCmd=%strLinker% /target:exe %strAppName%.js
+echo #%strCmd%
+%strCmd%
+
+pause
