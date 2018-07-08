3efeef99aa01e81e97364fee5cd806b81c0dc71c
asm32net <379754432@qq.com>
2018-07-08 17:41:14 +0800

    PaSwitchGitHubAccount2018JFrame.java ... add

---
+++ PaSwitchGitHubAccount2018JFrame.java/PaSwitchGitHubAccount2018JFrame.java
@@ -0,0 +1,7 @@
+// PaSwitchGitHubAccount2018JFrame.java
+
+class PaSwitchGitHubAccount2018JFrame{
+	public static void main(String[] args){
+		System.out.println("PaSwitchGitHubAccount2018JFrame.java");
+	}
+}
