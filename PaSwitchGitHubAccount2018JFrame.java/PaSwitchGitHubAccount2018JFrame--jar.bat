45c1fb1deb794f1c6f6566b49403d1d96a0b4ab7
asm32net <379754432@qq.com>
2018-07-10 01:50:02 +0800

    PaSwitchGitHubAccount2018JFrame--jar.bat ... add

---
+++ PaSwitchGitHubAccount2018JFrame.java/PaSwitchGitHubAccount2018JFrame--jar.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set strAppName=PaSwitchGitHubAccount2018JFrame
+echo Begin ...
+echo --------------------
+
+javac -d . %strAppName%.java
+echo --------------------
+echo Done.
+
+if exist %strAppName%.manifest.txt del /f %strAppName%.manifest.txt
+
+echo Manifest-Version: 1.0> %strAppName%.manifest.txt
+echo Created-By: 1.4.2_12 (Sun Microsystems Inc.)>> %strAppName%.manifest.txt
+echo Main-Class: %strAppName%>> %strAppName%.manifest.txt
+
+echo Release ...
+echo --------------------
+
+jar cvfm .\%strAppName%.jar .\%strAppName%.manifest.txt %strAppName%.class
+
+del %strAppName%.manifest.txt
+echo --------------------
+echo Done.
+
+pause
